3a006f26d24391fbd92c8a1d53cf34a324448b69
Matthieu 🇫🇷 <101651928+Mrdolls@users.noreply.github.com>
2025-12-26 19:13:37 +0100

    Update forb.sh

--- forb.sh
+++ forb.sh
@@ -7,7 +7,7 @@ else
     BOLD=""; GREEN=""; RED=""; YELLOW=""; BLUE=""; CYAN=""; NC=""
 fi
 
-VERSION="1.2"
+VERSION="1.4"
 INSTALL_DIR="$HOME/.forb"
 AUTH_FILE="$INSTALL_DIR/authorize.txt"
 UPDATE_URL="https://raw.githubusercontent.com/Mrdolls/forb/main/forb.sh"
